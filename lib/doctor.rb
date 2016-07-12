37429849f032374f685227536ddb64336e25e74e
Leah Nelson and Noah Yasskin <noah@>
2016-07-12 11:17:29 -0700

    write methods and specs for doctors

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -19,7 +19,23 @@ class Doctor
     doctors
   end
 
+  define_method(:save) do
+    result = DB.exec("INSERT INTO doctors (name, specialty_id) VALUES ('#{@name}', #{@specialty_id}) RETURNING id");
+    @id = result.first().fetch("id").to_i()
+  end
 
+  define_method(:==) do |another_doctor|
+    self.name().==(another_doctor.name()).&(self.id().==(another_doctor.id())).&(self.specialty_id().==(another_doctor.specialty_id()))
+  end
 
+  define_singleton_method(:find) do |id|
+    found_doctor = nil
+    Doctor.all().each() do |doctor|
+      if doctor.id().==(id)
+        found_doctor = doctor
+      end
+    end
+    found_doctor
+  end
 
 end
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -7,10 +7,52 @@ describe(Doctor) do
     end
   end
 
+  describe('#save') do
+    it("saves a new doctor to the database") do
+      test_doctor = Doctor.new({:id => nil, :name => "Fred", :specialty_id => 1})
+      test_doctor.save()
+      expect(Doctor.all()).to(eq([test_doctor]))
+    end
+  end
+
   describe("#name") do
     it("tells you the name of the doctor") do
-      test_doctor = Doctor.new({:id => nil, :name => "Fred", :specialty_id => nil})
+      test_doctor = Doctor.new({:id => nil, :name => "Fred", :specialty_id => 1})
       expect(test_doctor.name()).to(eq("Fred"))
+    end
+  end
+
+  describe("#id") do
+    it("tells you the id of the doctor") do
+      test_doctor = Doctor.new({:id => nil, :name => "Fred", :specialty_id => 1})
+      test_doctor.save()
+      expect(test_doctor.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe("#specialty_id") do
+    it("lets you read out the specialty id of the doctor") do
+      test_doctor = Doctor.new({:id => nil, :name => "Fred", :specialty_id => 1})
+      test_doctor.save()
+      expect(test_doctor.specialty_id()).to(eq(1))
+    end
+  end
+
+  describe("#==") do
+    it("is the same doctor if it has the same information") do
+      test_doctor = Doctor.new({:id => nil, :name => "Fred", :specialty_id => 1})
+      test_doctor2 = Doctor.new({:id => nil, :name => "Fred", :specialty_id => 1})
+      expect(test_doctor).to(eq(test_doctor2))
+    end
+  end
+
+  describe('.find') do
+    it('returns a doctor by an ID') do
+      test_doctor = Doctor.new({:id => nil, :name => "Fred", :specialty_id => 1})
+      test_doctor.save()
+      test_doctor2 = Doctor.new({:id => nil, :name => "Mary", :specialty_id => 2})
+      test_doctor2.save()
+      expect(Doctor.find(test_doctor2.id())).to(eq(test_doctor2))
     end
   end
 
